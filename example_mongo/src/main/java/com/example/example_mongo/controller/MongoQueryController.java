d7af4438635248b60238bd996842893f7cb45e2a
js3322 <js@cleancode.kr>
2023-08-31 07:08:31 +0900

    add : post example

--- example_mongo/src/main/java/com/example/example_mongo/controller/MongoQueryController.java
+++ example_mongo/src/main/java/com/example/example_mongo/controller/MongoQueryController.java
@@ -6,9 +6,9 @@ import com.mongodb.client.MongoCursor;
 import org.bson.Document;
 import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @RestController
@@ -20,11 +20,12 @@ public class MongoQueryController {
 
     //TODO [js] use mongo template and call mongodb data
     @GetMapping("/query")
-    public String executeQuery() {
+    public ResponseEntity<String> executeQuery() {
         MongoCollection<Document> collection = mongoTemplate.getCollection("TB_INDEX_TEST");
 
+        Document query = new Document("metadata.provider", "aws");
         // 예시: 모든 도큐먼트를 조회하는 쿼리
-        FindIterable<Document> documents = collection.find();
+        FindIterable<Document> documents = collection.find(query);
 
         JSONArray jsonArray = new JSONArray();
         MongoCursor<Document> cursor = documents.iterator();
@@ -33,6 +34,41 @@ public class MongoQueryController {
             jsonArray.put(doc.toJson());
         }
 
-        return jsonArray.toString();
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(jsonArray.toString());
+    }
+
+    @PostMapping("/query-post")
+    public ResponseEntity<String> executeQuery(@RequestBody RequestPayload payload) {
+        if(!"aws".equalsIgnoreCase(payload.getProvider())) {
+            return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body("[null]");
+        }
+
+        MongoCollection<Document> collection = mongoTemplate.getCollection("TB_INDEX_TEST");
+        Document query = new Document("metadata.provider", "aws");
+
+        JSONArray jsonArray = new JSONArray();
+        MongoCursor<Document> cursor = collection.find(query).iterator();
+        while (cursor.hasNext()) {
+            Document doc = cursor.next();
+            jsonArray.put(doc.toJson());
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(jsonArray.toString());
+    }
+}
+
+class RequestPayload {
+    private String provider;
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
     }
 }
