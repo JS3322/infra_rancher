6e0142f0dd48982e8cb7df4bc742828aca93fa2d
js3322 <js@cleancode.kr>
2023-09-02 13:41:43 +0900

    fix : swaager option text

--- api_gateway/src/main/kotlin/com/cleancode/api_gateway/config/SwaggerConfig.kt
+++ api_gateway/src/main/kotlin/com/cleancode/api_gateway/config/SwaggerConfig.kt
@@ -5,11 +5,15 @@ import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.context.annotation.Configuration
 
 //http://localhost:8080/swagger-ui/index.html
-
+//TODO [js] swagger info 및 설정
+/**
+ * swagger 설정
+ * @author js
+ */
 @OpenAPIDefinition(
     info = Info(title = "API gateway 명세서",
         description = "Batch Service API 명세서",
-        version = "0.0.1",
+        version = "0.1.1",
         )
 )
 @Configuration
--- api_gateway/src/main/kotlin/com/cleancode/api_gateway/controller/Route.kt
+++ api_gateway/src/main/kotlin/com/cleancode/api_gateway/controller/Route.kt
@@ -1,4 +1,8 @@
 package com.cleancode.api_gateway.controller
 
+/**
+ * 1차 라우팅 기능
+ * @author js
+ */
 class Route {
 }
